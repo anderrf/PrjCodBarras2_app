8dae35cbdf4d1150d2a521845b1209e66f6032d1
anderrf <andersonrochafarias@gmail.com>
2019-06-12 04:03:48 +0900

    Project's Initial Commit

---
+++ www/components/comandos.js
@@ -0,0 +1,9 @@
+// This is a JavaScript file
+
+$(document).on("click", "#btnCadastro", function(){
+  $(location).attr("href", "cadastro.html");
+});
+
+$(document).on("click", "#btnVoltar", function(){
+  $(location).attr("href", "index.html");
+});
