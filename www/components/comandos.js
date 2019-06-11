ea49d914e83d59c5dc6c406791b834b6d7657534
LuizGustavoMFerreira <luizmisslin@hotmail.com>
2019-06-12 04:49:10 +0900

    js comando para cadastrar

--- www/components/comandos.js
+++ www/components/comandos.js
@@ -11,3 +11,19 @@ $(document).on("click", "#btnVoltar", function(){
 $(document).on("click", "#btnPesquisa", function(){
   $(location).attr("href", "pesquisar.html");
 });
+
+$(document).on("click","#btnSalvar", function(){
+  var parametros = {
+    "nome": $("#nome").val(),
+    "cod": $("#cod").val(),
+    "valor": $("#valor").val(),
+    "descricao": $("#descricao").val(),
+    "processador": $("#processador").val(),
+    "sistemaop": $("#sistemaop").val(),
+    "tamtela": $("#tamtela").val(),
+    "tecwifi": $("#tecwifi").val(),
+    "qtcam": $("#qtcam").val(),
+    "rescam": $("#rescam").val(),
+    "memflash": $("#memflash").val()
+  }
+});
