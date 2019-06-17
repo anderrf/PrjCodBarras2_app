856401c02550410b1806225a50bc82a8469f86c8
anderrf <andersonrochafarias@gmail.com>
2019-06-18 02:49:31 +0900

    Adição dos comandos restantes, erro de lista-um e update

--- www/components/comandos.js
+++ www/components/comandos.js
@@ -31,7 +31,7 @@ $(document).on("click","#btnSalvar", function(){
 
   $.ajax({
     type: "post", //Como enviar
-    url: "https://bdscanweb-luizgustavo417.c9users.io/webservice/cadastrar.php", //Para onde enviar
+    url: "https://prjcodbarras-andersonrf.c9users.io/webservice/cadastrar.php", //Para onde enviar
     data: parametros, //O que enviar
     //Se der certo
     success: function (data){
@@ -61,7 +61,7 @@ $(document).on("click","#btnSalvar", function(){
 function pesquisa(){
    $.ajax({
         type:"post", //como enviar
-        url:"https://bdscanweb-luizgustavo417.c9users.io/webservice/lista.php",//para onde enviar
+        url:"https://prjcodbarras-andersonrf.c9users.io/webservice/lista.php",//para onde enviar
         dataType:"json",
         //se der certo
         success: function(data){
@@ -82,7 +82,7 @@ $(document).on("change","#pesquisa", function(){
    var codigoescolhido = $("option:selected", ("#pesquisa")).val();
     $.ajax({
         type:"get", //como enviar
-        url:"https://bdscanweb-luizgustavo417.c9users.io/webservice/lista-um.php",//para onde enviar
+        url:"https://prjcodbarras-andersonrf.c9users.io/webservice/lista-um.php",//para onde enviar
         data: "id="+codigoescolhido,
         dataType:"json",
           //se der certo
@@ -108,7 +108,6 @@ $(document).on("change","#pesquisa", function(){
 });
 
 function habilita(){
-  $("#id").prop("readonly",false);
   $("#nome").prop("readonly",false);
   $("#cod").prop("readonly",false);
   $("#valor").prop("readonly",false);
@@ -122,7 +121,6 @@ function habilita(){
   $("#memflash").prop("readonly",false);
 }
 function desabilita(){
-  $("#id").prop("readonly",true);
   $("#nome").prop("readonly",true);
   $("#cod").prop("readonly",true); 
   $("#valor").prop("readonly",true);
@@ -154,8 +152,8 @@ function listascan(resultado){
   var codigoescolhido = resultado;
     $.ajax({
         type:"get", //como enviar
-        url:"https://bdscanweb-luizgustavo417.c9users.io/webservice/lista-um.php",//para onde enviar
-        data: "id="+codigoescolhido,
+        url:"https://prjcodbarras-andersonrf.c9users.io/webservice/listascan.php",//para onde enviar
+        data: "cod="+codigoescolhido,
         dataType:"json",
           //se der certo
         success: function(data){
@@ -185,4 +183,52 @@ $(document).on("click", "#btnEditar", function(){
 
 $(document).on("click", "#btnCancelar", function(){
   desabilita();
+});
+
+$(document).on("click", "#btnSalvaAlt", function(){
+  var parametros = {
+        "nome": $("#nome").val(),
+        "cod": $("#cod").val(),
+        "valor": $("#valor").val(),
+        "descricao": $("#descricao").val(),
+        "processador": $("#processador").val(),
+        "sistemaop": $("#sistemaop").val(),
+        "tamtela": $("#tamtela").val(),
+        "tecwifi": $("#tecwifi").val(),
+        "qtcam": $("#qtcam").val(),
+        "rescam": $("#rescam").val(),
+        "memflash": $("#memflash").val()
+    };
+
+    $.ajax({
+        type:"post", //como enviar
+        url:"https://prjcodbarras-andersonrf.c9users.io/webservice/update.php",//para onde enviar
+        data:parametros,//o que enviar
+        //se der certo
+        success: function(data){
+            navigator.notification.alert(data);
+            location.reload();
+        },
+        //se der errado
+        error: function(data){
+             navigator.notification.alert(data);
+        }
+    }); 
+});
+
+$(document).on("click", "#btnDeletar", function(){
+  $.ajax({
+        type:"get", //como enviar
+        url:"https://prjcodbarras-andersonrf.c9users.io/webservice/deleta.php",//para onde enviar
+        data:"id="+$("#id").val(),
+        //se der certo
+        success: function(data){
+            navigator.notification.alert(data);
+            location.reload();//recarrega a pagina
+        },
+        //se der errado
+        error: function(data){
+             navigator.notification.alert(data);
+        }
+    }); 
 });
